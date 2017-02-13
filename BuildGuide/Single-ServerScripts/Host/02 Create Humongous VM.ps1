30b199f69d099f803829c7ce1bf0e7b0fd401de2
Rocky <Mark@BlueBuffaloPress.com>
2017-02-13 11:05:32 -0800

    Modified Build Script

--- BuildGuide/Single-ServerScripts/Host/02 Create Humongous VM.ps1
+++ BuildGuide/Single-ServerScripts/Host/02 Create Humongous VM.ps1
@@ -20,43 +20,15 @@ If (!( $isAdmin )) {
 } #END
 
 
-#TASK: VERIFYING THE CORRECT FILES ARE IN THE PROPER LOCATION
-Function Test-CorrectFiles {
-# Test for the moving of the correct images to the expected location else fail 
-
-$ISOSvr2016 = Get-ChildItem C:\RonsNotes\ISOs\Server2016\ | %{$_.Name} -ErrorAction inquire # ASK
-$ISOSql = Get-ChildItem C:\RonsNotes\ISOs\SQL2016\ | %{$_.Name} -ErrorAction inquire # ASK
-#$IMGSharePoint2016 = Get-ChildItem C:\RonsNotes\ISOs\Sharepoint2016\ | %{$_.Name} -ErrorAction inquire # ASK
-
-If ($ISOSvr2016 -eq "14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO")
-{Write-Host "Expected version of ISO for Server 2016 in expected location" -ForegroundColor Green}
-Else {Write-Host "Wrong or missing Server 2016 ISO. 14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO expected Correct to continue " -ForegroundColor Red; Read-Host -Prompt "Any key to continue" }
-
-If ($ISOSQL -eq "SQLServer2016-x64-ENU.iso")
-{Write-Host "Expected version of IMG for SQLServer 2016 in expected location" -ForegroundColor Green}
-Else {Write-Host "Wrong or missing SQL 2016 ISO. SQLServer2016-x64-ENU.iso expected. Correct to continue " -ForegroundColor Red ;  Read-Host -Prompt "Any key to continue"}
-
-<#If ($IMGSharePoint2016 -eq "officeserver.img")
-{Write-Host "Expected version of IMG for SharePoint Svr 2016 in expected location" -ForegroundColor Green}
-Else {Write-Host "Wrong or missing SharePoint.IMG. officeserver.img expected. Correct to continue " -ForegroundColor Red ;  Read-Host -Prompt "Any key to continue"}#>
-
-Write-host "*******"
-Write-host "*******"
-Write-host "*******"
-Write-host "*******"
-
-} #END
-Test-CorrectFiles
-
-
 #TASK: CREATE HUMONGOUS VIRTUAL MACHINE
+$SQLISOName = Get-ChildItem -Path 'C:\ISO\SQL\SQL 2016'
 Function Create-Humongous {
 $VMName = "Humongous"
 New-VHD  -Path "C:\RonsNotes\VM_Drives\Humongus\Humongus.vhdx" -SizeBytes 60GB -DynamicMemoryEnabled $false
 New-vm -Name $VMName -MemoryStartupBytes 8GB  -VHDPath "C:\RonsNotes\VM_Drives\Humongus\Humongus.vhdx" -BootDevice IDE -SwitchName "VMExternalNetwork" 
 Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -ControllerLocation 0 -Path C:\RonsNotes\ISOs\Server2016\14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO
 Set-vm $VMName -ProcessorCount 4 
-Add-VMDvdDrive -VMNAME $VMName -ControllerNumber 1 -ControllerLocation 1 -Path C:\RonsNotes\ISOs\SQL2016\SQLServer2016-x64-ENU.iso
+Add-VMDvdDrive -VMNAME $VMName -ControllerNumber 1 -ControllerLocation 1 -Path $SQLISOName.Name
 Start-VM  $VMName
 } #END
 Create-Humongous
